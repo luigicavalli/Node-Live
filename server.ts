7947936c82d81e6d4243411a6063242299632577
Luigi Cavalli <luigi_cavalli@outlook.com>
2024-09-20 10:10:19 +0200

    updated exercise

--- server.ts
+++ server.ts
@@ -36,11 +36,32 @@ app.post("/todos", (req: Request, res: Response) => {
 });
 
 app.put("/todos", (req: Request, res: Response) => {
-  res.end();
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(400).json({ errors: error.details.map((message) => message) });
+  } else {
+    const index = todos.findIndex((todo) => {
+      return todo.id === value.id;
+    });
+    if (index !== -1) {
+      todos[index] = value;
+    } else {
+      res.status(404).json({ message: "Todo non trovato" });
+    }
+    res.status(200).json({ message: "Todo inserito con successo." });
+  }
 });
 
 app.delete("/todos", (req: Request, res: Response) => {
-  res.end();
+  const index = todos.findIndex((todo) => {
+    return todo.id === Number(req.query.id);
+  });
+  if (index !== -1) {
+    todos.splice(index, 1);
+    res.status(200).json({ message: "Todo eliminato con successo." });
+  } else {
+    res.status(404).json({ message: "Todo non trovato" });
+  }
 });
 
 app.listen(port, () => {
